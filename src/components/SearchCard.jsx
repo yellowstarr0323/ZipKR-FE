b282d34f756de7e0098d5b18b77c7aaca54fbc66
hansaem <ihansaem60@gmail.com>
2026-09-16 21:39:15 +0900

    그림자 효과 추가

--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -14,16 +14,18 @@ export default function SearchCard() {
 
 const SearchCardWrapper = styled.div`
   width: 480px;
-  border: 1px gray solid;
+  border: 1px #DDDDDD solid;
   box-sizing: border-box;
   border-radius: 20px;
   display: flex;
   flex-direction: column;
+  background-color: #FFFFFF;
+  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.02), 0 2px 6px 0 rgba(0, 0, 0, 0.04), 0 4px 8px 0 rgba(0, 0, 0, 0.10);
 `
 
 const Line = styled.hr`
   width: 100%;
   margin: 0;
   border: none;
-  border-top: 1px solid gray;
+  border-top: 1px #DDDDDD solid;
 `
