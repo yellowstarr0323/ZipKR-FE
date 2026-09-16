3ccc661e3d61daedce88787fedbc368c8c998bde
hansaem <ihansaem60@gmail.com>
2026-09-16 22:46:39 +0900

    최근 검색한 주소 퍼블리싱

---
+++ src/components/RecentSearchBox.jsx
@@ -0,0 +1,72 @@
+import styled from "styled-components"
+import clockIcon from "../asset/icon-clock-512.png"
+
+export default function RecentSearchBox({address, postalCode}) {
+
+  return (
+    <RecentSearchBoxWrapper>
+
+      <RecentSearchAddressWrapper>
+        <ClockIconWrapper>
+          <ClockIcon src={clockIcon}/>
+        </ClockIconWrapper>
+        <RecentSearchAddress>{address}</RecentSearchAddress>
+      </RecentSearchAddressWrapper>
+      
+      <RecentSearchAddressPostalCode>{postalCode}</RecentSearchAddressPostalCode>
+    </RecentSearchBoxWrapper>
+  )
+}
+
+const RecentSearchBoxWrapper = styled.div`
+  display: flex;
+  padding: 19px 4px;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 8px;
+  &:hover{
+    background-color: #f8f8f8;
+  }
+`
+
+const ClockIcon = styled.img`
+  width: 13px;
+  height: 13px;
+  flex-shrink: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const RecentSearchAddress = styled.p`
+  color: #222;
+  font-family: Inter;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+const RecentSearchAddressPostalCode = styled.p`
+  color: #6A6A6A;
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+const RecentSearchAddressWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`
+
+const ClockIconWrapper = styled.div`
+  border-radius: 9999px;
+  background: #F2F2F2;
+  width: 28px;
+  height: 28px;
+  position: relative;
+`
--- src/components/RecentSearchList.jsx
+++ src/components/RecentSearchList.jsx
@@ -1,3 +1,70 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import RecentSearchBox from "./RecentSearchBox";
+
 export default function RecentSearchList() {
 
+  const [recentSearchList, setRecentSearchList] = useState([
+    {
+      address: "세종특별자치시 한누리대로 2130",
+      postalCode: "30151"
+    },
+    {
+      address: "서울특별시 강남구 테헤란로 123",
+      postalCode: "06133"
+    }
+  ]);
+
+
+  return (
+    <RecentSearchListWrapper>
+      <RecentSearchText>최근 검색</RecentSearchText>
+
+      {recentSearchList.length > 0
+        ? recentSearchList.map((recentSearch) => (
+          <RecentSearchBox
+            key={recentSearch.postalCode}
+            address={recentSearch.address}
+            postalCode={recentSearch.postalCode}
+          />
+        ))
+        : (
+          <IntroduceTextWrapper>
+            <IntroduceText>
+              도로명, 건물명, 지번 중 아무거나 입력하면 됩니다.
+            </IntroduceText>
+          </IntroduceTextWrapper>
+        )
+      }
+
+    </RecentSearchListWrapper>
+
+  )
 }
+
+const RecentSearchListWrapper = styled.div`
+  padding-left: 16px;
+  padding-top: 4px;
+  padding-bottom: 16px;
+  padding-right: 16px;
+`
+
+const RecentSearchText = styled.p`
+  font-size: 11px;
+  padding-top:5px;
+  padding-bottom: 5px;
+`
+
+const IntroduceText = styled.p`
+  color: #6A6A6A;
+  font-family: Inter;
+  font-size: 13px;
+`
+
+const IntroduceTextWrapper = styled.div`
+  display: flex;
+  padding: 22px 4px 13px 4px;
+  flex-direction: column;
+  align-items: flex-start;
+  align-self: stretch;
+`
